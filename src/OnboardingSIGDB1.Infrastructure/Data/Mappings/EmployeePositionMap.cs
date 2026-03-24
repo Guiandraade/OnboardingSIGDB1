98037e4c2082db02d74ea1564ee3655e341328d2
guilherme.nascimento <guiandrade.na@gmail.com>
2026-03-24 11:05:31 -0300

    feat: add infrastructure mappings for domain entities

---
+++ src/OnboardingSIGDB1.Infrastructure/Data/Mappings/EmployeePositionMap.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnboardingSIGDB1.Domain.Entities;
+
+namespace OnboardingSIGDB1.Infrastructure.Data.Mappings;
+
+public class EmployeePositionMap : IEntityTypeConfiguration<EmployeePosition>
+{
+    public void Configure(EntityTypeBuilder<EmployeePosition> builder)
+    {
+        builder.ToTable("EmployeePositions");
+        
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id).ValueGeneratedOnAdd();
+
+        builder.Property(e => e.StartDate)
+            .IsRequired();
+        
+        builder.Property(e => e.CreatedAt)
+            .IsRequired();
+        
+        builder.HasOne(e => e.Employee)
+            .WithMany(e => e.Positions)
+            .HasForeignKey(e => e.EmployeeId)
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        builder.HasOne(e => e.Position)
+            .WithMany(p => p.EmployeePositions)
+            .HasForeignKey(e => e.PositionId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
