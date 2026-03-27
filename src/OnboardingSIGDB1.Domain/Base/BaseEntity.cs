b8a57a852822d5593a090fba2b0fdab1dfb2c917
guilherme.nascimento <guiandrade.na@gmail.com>
2026-03-27 14:07:45 -0300

    refactor/base-entity-element-inheritance

--- src/OnboardingSIGDB1.Domain/Base/BaseEntity.cs
+++ src/OnboardingSIGDB1.Domain/Base/BaseEntity.cs
@@ -3,23 +3,7 @@ using OnboardingSIGDB1.Domain.Notifications;
 
 namespace OnboardingSIGDB1.Domain.Base;
 
-public abstract class BaseEntity<T> : AbstractValidator<T> where T : class
+public abstract class BaseEntity<T> : BaseElement<T> where T : class
 {
     public int Id { get; protected set; }
-    public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
-    
-    private readonly List<Notification> _notifications = new();
-    public IReadOnlyCollection<Notification> Notifications => _notifications.AsReadOnly();
-    
-    public bool IsValid => !_notifications.Any();
-    
-    public abstract bool Validation();
-    
-    protected void AddNotification(string key, string message)
-    {
-        if (!_notifications.Any(n => n.Key == key && n.Message == message))
-            _notifications.Add(new Notification(key, message));
-    }
-    
-    protected void ClearNotifications() => _notifications.Clear();
 }
