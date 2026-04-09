08f03146083f1f4e0478de6a3b910f467974f598
guilherme.nascimento <guiandrade.na@gmail.com>
2026-04-09 09:49:17 -0300

    feat(domain): add company foundation date validation and format standardization

--- src/OnboardingSIGDB1.Domain/Dto/EmployeeAndPositions/Response/PositionHistoryItemResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OnboardingSIGDB1.Domain.Dto.EmployeeAndPositions.Response;
-
-public record PositionHistoryItemResponse
-{
-    public string PositionDescription { get; init; }
-    public DateTime StartDate { get; init; }
-    public DateTime? EndDate { get; init; }
-}
