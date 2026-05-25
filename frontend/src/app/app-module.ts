b53696b0d377f25a74ddba0fae4f9ff16031caae
guilherme.nascimento <guiandrade.na@gmail.com>
2026-05-25 17:17:44 -0300

    feat(frontend): implement CRUD functionality for positions with form validation

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -10,7 +10,7 @@ import { PositionForm } from './features/positions/position-form/position-form';
 
 @NgModule({
   declarations: [App, PositionList, PositionForm],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [App]
 })
--- frontend/src/app/features/positions/position-list/position-list.ts
+++ frontend/src/app/features/positions/position-list/position-list.ts
@@ -1,8 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PositionFilter, PositionResponse } from 'src/app/core/models/position.model';
+import { PositionService } from 'src/app/core/services/position.service';
+import { Notification } from 'src/app/core/models/pagination.model';
 
 @Component({
   selector: 'app-position-list',
   templateUrl: './position-list.html',
   styleUrls: ['./position-list.css']
 })
-export class PositionList {}
+export class PositionList implements OnInit {
+
+  positions: PositionResponse[] = [];
+  isLoading = false;
+  errorMessage = '';
+  filter: PositionFilter = {
+    pageNumber: 1,
+    pageSize: 10,
+  };
+  totalItems = 0;
+  totalPages = 0;
+
+  constructor(private positionService: PositionService) { }
+
+  ngOnInit(): void {
+    this.getPositions();
+  }
+
+  getPositions(): void {
+    this.isLoading = true;
+    this.positionService.getPositions(this.filter).subscribe({
+      next: (response) => {
+        this.totalItems = response.total;
+        this.totalPages = Math.ceil(response.total / this.filter.pageSize!);
+        this.positions = response.data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Error fetching positions:', err);
+        if (Array.isArray(err.error)) {
+          this.errorMessage = err.error.map((n: Notification) => n.message).join(', ');
+        } else {
+          this.errorMessage = 'Error fetching positions';
+        }
+        this.isLoading = false;
+      }
+  });
+  }
+
+  deletePosition(id: number): void {
+    if (confirm('Are you sure you want to delete this position?')) {
+      this.isLoading = true;
+      this.positionService.deletePosition(id).subscribe({
+        next: () => {
+          this.getPositions();
+        },
+        error: (err) => {
+          console.error('Error deleting position:', err);
+          if (Array.isArray(err.error)) {
+            this.errorMessage = err.error.map((n: Notification) => n.message).join(', ');
+          } else {
+            this.errorMessage = 'Error deleting position';
+          }
+          this.isLoading = false;
+        }
+      });
+    }
+  }
+
+  get currentPage(): number {
+  return this.filter.pageNumber ?? 1;
+}
+
+  search(): void {
+    this.filter.pageNumber = 1;
+    this.getPositions();
+  }
+
+  changePage(page: number): void {
+    this.filter.pageNumber = page;
+    this.getPositions();
+  }
+}
